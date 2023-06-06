7a109b7011131086b10781ec4409ba455f361df2
KingDam <akqjq9250@gmail.com>
2023-06-06 22:34:48 +0900

    edit

--- 기말프로젝트/decode.c
+++ 기말프로젝트/decode.c
@@ -5,6 +5,12 @@
 
 char* encodedFilename;
 
+typedef struct{
+                FILE* fp;
+                char** all;
+                int maxCol;
+}FileData;
+
 // 2의 제곱수 구하는 함수
 int pow2(int value) {
                 int result = 1;
@@ -37,12 +43,12 @@ char output_origin(unsigned short result) {
                 }
                 // printf("check : %d\n", check);
                 if (check != 0) {
-                                if (haming[12-check] == 0) haming[12-check] = 1;
-                                else if (haming[12-check] == 1) haming[12-check] = 0;
+                                if (haming[12 - check] == 0) haming[12 - check] = 1;
+                                else if (haming[12 - check] == 1) haming[12 - check] = 0;
                 }
                 /*       printf("check befor\n");
-                                                for(i = 0; i < 12; i++)
-                                                printf("%d ", haming[i]);
+                                                 for(i = 0; i < 12; i++)
+                                                 printf("%d ", haming[i]);
                                                  printf("\n");  */
                 for (i = 0; i < 12; i++) {
                                 if (i == 11 || i == 10 || i == 8 || i == 4)
@@ -54,49 +60,180 @@ char output_origin(unsigned short result) {
                 }
 
                 /* for(i = 0; i < 8; i++)
-                        printf("%d ", bin[i]);
-                        printf("\n");*/
+                         printf("%d ", bin[i]);
+                         printf("\n");*/
 
                 for (i = 0; i < 8; i++)
                                 output += bin[i] * pow2(7 - i);
                 return output;
 }
 
-void view(FILE* fp, char** all) {
+void view(FileData* fd, int col) {
                 char arr2[16];
                 unsigned short arr[12];
                 char storage[16][12];
                 int i, j;
-                int num = fread(arr, sizeof(unsigned short), 12, fp);
+                int num = fread(arr, sizeof(unsigned short), 12, fd->fp);
 
-                if (num == 0)
+                if (num == 0) {
                                 return;
+                }
 
                 for (i = 0; i < 12; i++) {
                                 for (j = 0; j < 16; j++)
                                                 storage[j][i] = arr[i] >> (15 - j) & 1;
                 }
-                view(fp);
+                fd->maxCol = col;
+                view(fd, col + 1);
+
+                if (fd->all == NULL) {
+                                fd->all = (char**)malloc(sizeof(char*) * fd->maxCol);
+                }
+
+                fd->all[fd->maxCol - col] = (char*)malloc(sizeof(char) * 16);
 
                 for (i = 0; i < 16; i++) {
                                 unsigned short result = 0;
                                 for (j = 0; j < 12; j++)
                                                 result += (storage[i][j] & 1) * pow2(11 - j);
-                                arr2[i] = output_origin(result);
+                                fd->all[fd->maxCol - col][i] = output_origin(result);
                 }
+
                 for (i = 0; i < 16; i++)
-                                printf("%c", arr2[i]);
+                                printf("%c", fd->all[fd->maxCol - col][i]);
                 printf("\n");
 }
 
-int main(int argc, char* argv[]) {
-                char* filename=argv[1];
-                encodedFilename=argv[2];
-                FILE* fp = fopen(filename,"rt");
+void clearStr(char str[]){
+                int size=strlen(str);
+                for(int i=0;i<size;i++) str[i]=0;
+}
 
-                char** all=(char**)malloc(sizeof(char*)*16);
-                view(fp, all);
-                fclose(fp);
+void readAll(FileData* fd){
+                int USS=0;//max=7
+                int ITS=0;//max=1
+                int FRS=0;//max=4 백틱 없으면 초기화
+                // int DSS;
+                int SCnt=0;
+                char str[256];
+                int index=0;
+                int RealFirst=1;
+                int fCnt=0;
+
+                printf("*USER STATUS*\n");
+                for(int i=0;i < fd->maxCol;i++){
+                                for(int j=0;j<16;j++){
+                                                if(fd->all[i][j]!='/') str[index++]=fd->all[i][j];
+                                                if(fd->all[i][j]=='/'){
+                                                                if(USS<=7){
+                                                                                USS++;
+                                                                                switch(USS){
+                                                                                                case 1 :
+                                                                                                                printf("ID: %s\n", str);
+                                                                                                                break;
+                                                                                                case 2:
+                                                                                                                printf("NAME: %s\n", str);
+                                                                                                                break;
+                                                                                                case 3:
+                                                                                                                printf("GENDER: %s\n", str);
+                                                                                                                break;
+                                                                                                case 4:
+                                                                                                                printf("AGE: %s\n", str);
+                                                                                                                break;
+                                                                                                case 5:
+                                                                                                                printf("HP: %s\n", str);
+                                                                                                                break;
+                                                                                                case 6:
+                                                                                                                printf("MP: %s\n", str);
+                                                                                                                break;
+                                                                                                case 7:
+                                                                                                                printf("COIN: %s\n", str);
+                                                                                                                break;
+                                                                                }
+                                                                                clearStr(str);
+                                                                }
+                                                                else if(USS==8&&ITS==0){
+                                                                                printf("*ITEMS*\n");
+                                                                                ITS++;
+                                                                                int size=strlen(str);
+                                                                                for(int i=0;i<size;i++){
+                                                                                                char num[4]; int numCnt=0;
+                                                                                                if(str[i]>='0'&&str[i]<='9'){
+                                                                                                                num[numCnt++]=str[i];
+                                                                                                                continue;
+                                                                                                }
+                                                                                                else{
+                                                                                                                switch(str[i]){
+                                                                                                                                case 'A':
+                                                                                                                                  printf("BOMB: %s\n", num);
+                                                                                                                                  break;
+                                                                                                                                case 'B':
+                                                                                                                                  printf("POTION: %s\n", num);
+                                                                                                                                  break;
+                                                                                                                                case 'C':
+                                                                                                                                  printf("CURE: %s\n", num);
+                                                                                                                                  break;
+                                                                                                                                case 'D':
+                                                                                                                                  printf("BOOK: %s\n", num);
+                                                                                                                                  break;
+                                                                                                                                case 'E':
+                                                                                                                                  printf("SHIELD: %s\n", num);
+                                                                                                                                  break;
+                                                                                                                                case 'F':
+                                                                                                                                  printf("CANNON: %s\n", num);
+                                                                                                                                  break;
+                                                                                                                }
+                                                                                                                clearStr(num);
+                                                                                                }
+                                                                                }
+                                                                                clearStr(str);
+                                                                }
+                                                                else if(str=="`") FRS=5;
+                                                                else if(ITS==1&&FRS<=4){
+                                                                                FRS++; fCnt++;
+                                                                                if(RealFirst){
+                                                                                                printf("*FRIENDS LIST*\n");
+                                                                                                RealFirst=0;
+                                                                                }
+                                                                                switch(FRS){
+                                                                                                case 1:
+                                                                                                                printf("FRIENDS%d ID: %s\n", fCnt, str);
+                                                                                                                break;
+                                                                                                case 2:
+                                                                                                                printf("FRIENDS%d NAME: %s\n", fCnt, str);
+                                                                                                                break;
+                                                                                                case 3:
+                                                                                                                printf("FRIENDS%d GENDER: %s\n", fCnt, str);
+                                                                                                                break;
+                                                                                                case 4:
+                                                                                                                printf("FRIENDS%d AGE: %s\n", fCnt, str);
+                                                                                                                FRS=0;
+                                                                                                                break;
+                                                                                }
+                                                                                clearStr(str);
+                                                                }
+                                                                else if(FRS==5&&strcmp(str,"~")!=0){
+                                                                                printf("%s\n", str);
+                                                                }
+                                                }
+
+                                }
+
+                }
+
+}
+int main(int argc, char* argv[]) {
+                char* filename = argv[1];
+                encodedFilename = argv[2];
+
+                FileData* fd = (FileData*)malloc(sizeof(FileData));
+                fd->fp = fopen(filename, "rb");
+                int col = 1;
+
+                fd->all = NULL;
+                view(fd, col);
+                readAll(fd);
+                fclose(fd->fp);
 
                 return 0;
 }
